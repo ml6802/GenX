e0683c7d848d29d056d3b7dde99468fc68634898
Gabriel A. Mantegna <gm1710@della8.princeton.edu>
2024-08-29 06:49:31 -0400

    increase slack to 100

--- src/model/core/ldes_slack.jl
+++ src/model/core/ldes_slack.jl
@@ -6,7 +6,7 @@ function lds_slack!(EP::Model, inputs::Dict,setup::Dict)
 
 	@constraint(EP,cPosSlack[w in 1:inputs["REP_PERIOD"]],vLDS_SLACK_MAX[w]>=0)
     
-    PenaltyValue = 10*(inputs["Weights"]/inputs["H"])*inputs["Voll"][1] ;    
+    PenaltyValue = 100*(inputs["Weights"]/inputs["H"])*inputs["Voll"][1] ;    
 
 	@expression(EP,eObjSlack,sum(PenaltyValue[w]*vLDS_SLACK_MAX[w] for w in 1:inputs["REP_PERIOD"]))
     
