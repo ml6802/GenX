4929ab27b9c7b41b7ca03de1f013b699d1c6d933
David Cole <dlcdavidc@gmail.com>
2025-05-29 17:10:31 -0400

    Added basic 4-bus case

--- example_systems/Simple_four_bus_DC_OPF/run_gurobi.jl
+++ example_systems/Simple_four_bus_DC_OPF/run_gurobi.jl
@@ -4,10 +4,10 @@ using GenX
 using Gurobi
 using JuMP
 
-m = run_genx_case!(dirname(@__FILE__), Gurobi.Optimizer)
+m, inputs = run_genx_case!(dirname(@__FILE__), Gurobi.Optimizer)
 
 vars = all_variables(m)
 
-for var in vars
+for var in vars[80:100]
     println(var, "    ", value(var))
 end
