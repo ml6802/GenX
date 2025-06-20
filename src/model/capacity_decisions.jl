72dacd93dfac11d477fa155beca3d5541a4f3865
Mike Lau <mike_@dynamic-oit-ip4-wifirestricted04-10-50-78-96.princeton.edu>
2025-06-20 15:14:31 -0400

    Benders PTDF version working

--- src/model/capacity_decisions.jl
+++ src/model/capacity_decisions.jl
@@ -178,6 +178,15 @@ function transmission_capacity_decisions!(EP, inputs::Dict, setup::Dict)
                 end
                 inputs["EXPANSION_LEVELS"] = EXPANSION_LEVELS
                 @variable(EP, vNEW_TRANS_CAP[l in EXPANSION_LINES]>=0)
+            elseif setup["ptdf"] == 1
+                REINFORCEMENT_CAP_SIZE = inputs["Line_Reinforcement_Cap_Size"]
+                MAX_TRAN_EXPANSION_LIMIT=inputs["Max_Trans_Cap"]
+                EXPANSION_LEVELS=Dict{Int,Vector{Float64}}()
+                for l in EXPANSION_LINES
+                    EXPANSION_LEVELS[l] = (0:1:MAX_TRAN_EXPANSION_LIMIT[l]) #-Might not need multiplication of this part -->* REINFORCEMENT_CAP_SIZE[l]
+                end
+                inputs["EXPANSION_LEVELS"] = EXPANSION_LEVELS
+                @variable(EP, vZ_BUILD[l in EXPANSION_LINES, i in 1:(inputs["Max_Trans_Cap"][l])] in Parameter(0)) #PTDF variable
             else
                 @variable(EP, vNEW_TRANS_CAP_DECISION_INT[l in EXPANSION_LINES, i in 1:inputs["Max_Trans_Cap"][l]], Bin)
                 REINFORCEMENT_CAP_SIZE = inputs["Line_Reinforcement_Cap_Size"]
@@ -216,6 +225,14 @@ function transmission_capacity_decisions!(EP, inputs::Dict, setup::Dict)
                 else
                     eTransMax[l]
                 end)
+            elseif setup["ptdf"] == 1
+                @expression(EP, eAvail_Trans_Cap[l = 1:L],
+                    if l in EXPANSION_LINES
+                        eTransMax[l] + sum(vZ_BUILD[l, i] for i in 1:(inputs["Max_Trans_Cap"][l])) * inputs["Line_Reinforcement_Cap_Size"][l] #TODO: Make sure this "l" is the correct index
+                    else
+                        eTransMax[l]
+                    end
+                )
             else
                 @expression(EP, eAvail_Trans_Cap[l = 1:L],
                 if l in EXPANSION_LINES
