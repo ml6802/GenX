28e8fd6f34398b478983742d39911708bb9fee04
Chakrabarti, Sambuddha (Sam) <sambuddha.chakrabarti@gmail.com>
2025-09-04 13:35:21 -0400

    Added InvestmentScheduleResults struct to test_portfolio for output pipelining

--- example_systems/RTS_Case_Latest/test_portfolio.jl
+++ example_systems/RTS_Case_Latest/test_portfolio.jl
@@ -929,6 +929,29 @@ mysetup["NetworkExpansion"] = 1
 EP = GenX.generate_model(mysetup, myinputs, solver)
 GenX.optimize!(EP)
 
+"""
+    InvestmentScheduleResults
+
+A mutable struct that stores the results of investment decisions over multiple investment periods.
+
+# Fields
+
+  - `results::Dict`: Dictionary mapping investment periods to technology investment results.
+    Structure: InvestmentPeriod => (TypeTechnology, "name") => BuildCapacity
+    Another idea would be the structure to be:
+    Structure: InvestmentPeriod => TypeTechnology => Dict{String, BuildCapacity}
+
+    Where:
+
+      + InvestmentPeriod: Time period when investment decisions are made
+      + TypeTechnology: The type of technology being invested in
+      + "name": String identifier for the specific technology instance
+      + BuildCapacity: The capacity to be built for that technology in that period
+"""
+mutable struct InvestmentScheduleResults 
+    results::Dict{Tuple{Date, Date}, Dict{Tuple{DataType, String}, Any}} # InvestmentPeriod => (TypeTechnology, "name") => BuildCapacity
+end
+
 # Extract results and write capacities back to portfolio
 function extract_genx_results_to_psip_format(EP, myinputs, p)
     """
