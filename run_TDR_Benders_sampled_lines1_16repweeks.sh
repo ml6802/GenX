79afc397176ea42e4b52bd35c73630b977b1537b
David Cole <dlcdavidc@gmail.com>
2025-12-18 11:45:49 -0500

    Fixed missing call

--- run_TDR_Benders_sampled_lines1_16repweeks.sh
+++ run_TDR_Benders_sampled_lines1_16repweeks.sh
@@ -12,4 +12,4 @@
 module purge
 module load gurobi/12.0.0
 module load julia/1.10.5
-julia -t 32 --project=./ ./example_systems/RTS_Case_Latest/TDR_monolithic_sampled_lines1_16repweeks_LP.jl
+julia -t 32 --project=./ ./example_systems/RTS_Case_Latest/TDR_Benders_sampled_lines1_16repweeks.jl
