1cd9831b75c51472e9200f80e223d8eeddc1ad22
David Cole <dlcdavidc@gmail.com>
2025-12-19 12:48:53 -0500

    Added test for old susceptance values

---
+++ example_systems/RTS_Case_Latest/TDR_benders_sampled_lines1_old_susceptance.jl
@@ -0,0 +1,208 @@
+ENV["GENX_PRECOMPILE"] = "false"
+
+using Revise
+using JuMP
+using GenX
+using PowerSystemsInvestmentsPortfolios
+using Gurobi
+using TimeSeries
+using CSV
+using DataFrames
+using Dates
+using InfrastructureSystems
+using PowerSystems
+const PSIP = PowerSystemsInvestmentsPortfolios
+const IS = InfrastructureSystems
+const PSY = PowerSystems
+using Plots
+import Pkg
+using Distributed, ClusterManagers
+using Random
+
+
+# Load in portfolio
+include((@__DIR__)*"/load_portfolio.jl")
+# Load in functions for downscaling
+include((@__DIR__)*"/../convert_input_dict_reconductoring.jl")
+# Load in functions for adding candidate lines
+include((@__DIR__)*"/load_candidate_line_functions.jl")
+
+# Set internal portfolio data for use in GenX
+p.internal.ext["Rep_Periods"] = 1
+p.internal.ext["Timesteps_per_Rep_Period"] = 168
+p.internal.ext["hours_per_subperiod"] = 168
+p.internal.ext["sub_weights"] = [8784 for i in 1:p.internal.ext["Rep_Periods"]] 
+add_om_costs(p)
+
+techs = collect(get_technologies(SupplyTechnology, p))
+
+# Build map of buses to zones; used in downscaling
+buses = collect(get_components(Bus, p.base_system))
+zones = []
+zone_map = Dict{Int, Int}()
+for i in 1:length(buses)
+    if buses[i].area.name == "area1"
+        push!(zones, 1)
+        zone_map[i] = 1
+    elseif buses[i].area.name == "area2"
+        push!(zones, 2)
+        zone_map[i] = 2
+    elseif buses[i].area.name == "area3"
+        push!(zones, 3)
+        zone_map[i] = 3
+    else
+        error()
+    end
+end
+
+cpus_per_task = parse(Int, ENV["SLURM_CPUS_PER_TASK"]);
+addprocs(cpus_per_task)
+println("Adding processors")
+@everywhere begin
+    import Pkg
+    Pkg.activate("/scratch/gpfs/JENKINS/dc0173/git/forked/reconductoring/GenX")
+end
+
+println("Number of procs: ", nprocs())
+println("Number of workers: ", nworkers())
+for i in workers()
+    id, pid, host = fetch(@spawnat i (myid(), getpid(), gethostname()))
+    println(id, " " , pid, " ", host)
+end
+@everywhere using GenX, Distributed
+
+
+# Load in settings
+genx_settings = GenX.get_settings_path(case, "genx_settings.yml") # Settings YAML file path
+writeoutput_settings = GenX.get_settings_path(case, "output_settings.yml") # Write-output settings YAML file path
+mysetup = GenX.configure_settings(genx_settings, writeoutput_settings) # mysetup dictionary stores settings and GenX-specific parameters
+
+# Make sure certain parameters are set
+mysetup["ParameterScale"] = 0
+mysetup["DC_OPF"] = 1
+mysetup["ptdf"] = 0
+mysetup["bilinear"] = 0
+mysetup["disaggregate"] = 0
+mysetup["unfix_slacks"] = 0
+mysetup["SOS1"] = 0
+settings_path = GenX.get_settings_path(case)    
+mysetup["settings_path"] = settings_path;
+mysetup["NetworkExpansion"] = 1
+mysetup["Benders"] = 0
+
+node_names = ["Carew", "Chase", "Carrel", "Carter", "Cabot", "Bajer", "Baker", "Baffin", "Cabell", "Caine", "Camus", "Bach", "Bain", "Barlow", "Banks", "Balch", "Alger", "Alber", "Alder", "Avery", "Aiken"]
+
+# Split node names by initial letter (A, B, C)
+a_node_names = filter(n -> startswith(n, "A"), node_names)
+b_node_names = filter(n -> startswith(n, "B"), node_names)
+c_node_names = filter(n -> startswith(n, "C"), node_names)
+
+# Set a reproducible seed (outside the function)
+function sample_four(names::AbstractVector{<:AbstractString})
+    @assert length(names) >= 3 "Need at least 4 names (got $(length(names)))"
+    idxs = sort(randperm(length(names))[1:4])
+    return collect(names[idxs])
+end
+node_name_dict = Dict('A' => a_node_names, 'B' => b_node_names, 'C' => c_node_names)
+techs = collect(get_technologies(ResourceTechnology, p))
+
+Random.seed!(1234)
+for i in 1:length(techs)
+    t = techs[i]
+    if length(t.region) > 1
+        old_region_list = t.region
+        new_region_list = PSIP.RegionTopology[]
+        key = t.region[1].name[1]
+        node_name_vector = node_name_dict[key]
+        new_nodes = sample_four(node_name_vector)
+        for name in new_nodes
+            for n in old_region_list
+                if n.name == name
+                    push!(new_region_list, n)
+                    break
+                end 
+            end
+        end
+        @assert length(new_region_list) >=1
+        t.region = new_region_list
+    end
+end
+
+myinputs = GenX.load_inputs(mysetup, case, p)
+
+optimizer = optimizer_with_attributes(Gurobi.Optimizer, "TimeLimit" => 64800, "MIPGap" => 1e-3)
+
+# Add expected candidate line data
+# also scales demands up by 2x
+load_candidates_base(myinputs, 8784, add_new_corridors = true)
+
+for i in myinputs["CAN_RETIRE_LINES"]
+    myinputs["pDC_OPF_coeff"][120 + i] /= 2.5
+end
+update_fuel_and_investment_costs(myinputs)
+
+using StatsBase, Random
+Random.seed!(123)
+CANDIDATE_LINES = myinputs["CANDIDATE_LINES"]
+RECONDUCTOR_LINES = myinputs["RECONDUCTOR_LINES"]
+lines_to_keep = sample(CANDIDATE_LINES, 40, replace=false, ordered=true)
+for i in [241, 242, 243, 244, 245, 246]
+    push!(lines_to_keep, i)
+end
+println("LINES TO KEEP ARE: ")
+println(lines_to_keep)
+
+lines_to_keep_reconductor = sample(RECONDUCTOR_LINES, 25, replace=false, ordered=true)
+myinputs["RECONDUCTOR_LINES"] = lines_to_keep_reconductor
+
+GenX.filter_candidate_lines(myinputs, lines_to_keep)
+
+println("NUMBER OF POSSIBLE LINE RETIREMENTS: ", length(myinputs["CAN_RETIRE_LINES"]))
+
+
+# Run TDR
+cluster_inputs(case, settings_path, mysetup; inputs = myinputs)
+
+GenX.expand_new_cap_resources_to_nodal!(myinputs, mysetup, p, "")
+
+mysetup["IntegerInvestments"] = 1
+mysetup["DC_OPF"] = 0
+mysetup["NetworkExpansion"] = 1
+
+if haskey(mysetup, "IntegerInvestments")
+    if mysetup["IntegerInvestments"] == 1
+        for i in myinputs["NEW_CAP"]
+            resource = myinputs["RESOURCES"][i]
+            parent(resource)[:cap_size] = 200
+        end
+    end
+end
+scale_inputs(myinputs)
+
+benders_settings_path = GenX.get_settings_path(case, "benders_settings.yml")
+mysetup_benders = GenX.configure_benders(benders_settings_path) 
+mysetup = merge(mysetup,mysetup_benders);
+
+
+mysetup["NetworkExpansion"] = 1
+mysetup["Benders"] = 1
+mysetup["bilinear"] = 1
+mysetup["DC_OPF"] = 1
+mysetup["IntegerInvestments"] = 1
+mysetup["BD_integer_routine"] = 0
+mysetup["BD_cap_integer_routine"] = 0
+
+myinputs_decomp = GenX.separate_inputs_subperiods(myinputs);
+benders_inputs = GenX.generate_benders_inputs(mysetup,myinputs,myinputs_decomp)
+
+planning_problem1, planning_sol1, operational_sol1, LB_hist1,UB_hist1, cpu_time1,feasibility_hist1, build_decisions1  = GenX.benders(benders_inputs,mysetup,myinputs);
+
+
+println("RUNNING 1 Month")
+# println(operational_sol1.summation_map)
+
+for i in keys(planning_sol1.values)
+    if planning_sol1.values[i] != 0
+        println(i, " = ", planning_sol1.values[i])
+    end
+end
