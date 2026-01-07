2627a9a95ecf9a9989b1612140f78e3286ebe7ee
David Cole <dlcdavidc@gmail.com>
2026-01-07 10:12:22 -0500

    Updated investment costs

--- example_systems/RTS_Case_Latest/TDR_benders_16repweeks.jl
+++ example_systems/RTS_Case_Latest/TDR_benders_16repweeks.jl
@@ -135,6 +135,7 @@ optimizer = optimizer_with_attributes(Gurobi.Optimizer, "TimeLimit" => 64800, "M
 # Add expected candidate line data
 # also scales demands up by 2x
 load_candidates_base(myinputs, 8784, add_new_corridors = true)
+update_fuel_and_investment_costs(myinputs)
 
 # Run TDR
 TDR_params = Dict("MinPeriods" => 16, "MaxPeriods" => 16, "UseExtremePeriods" => 1)
