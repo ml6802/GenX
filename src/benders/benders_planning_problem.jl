0141a9198803c76c811976edb1568009250e7223
David Cole <dlcdavidc@gmail.com>
2026-01-07 14:25:04 -0500

    Updated to support waterflow model

--- src/benders/benders_planning_problem.jl
+++ src/benders/benders_planning_problem.jl
@@ -138,28 +138,28 @@ function solve_planning_problem(EP::Model,planning_variables::Vector{String},inp
 				zone_inv_cost = 0#make_benders_zonal_invcost(inputs, EP)
 				planning_sol =  (LB = objective_value(EP), inv_cost =value(EP[:eObj]), zone_inv_cost = zone_inv_cost,values =Dict([s=>value.(variable_by_name(EP,s)) for s in planning_variables]), theta = value.(EP[:vTHETA])) 
 
-				total_builds = [0.]
-				for var in EP[:vNEW_TRANS_CAP_DECISION_INT]
-					if value(var) > 0
-						#println(var)
-						total_builds[1] += value(var)
-					end
-				end
-				println("NUMBER OF BUILDS IS ", total_builds[1])
+				# total_builds = [0.]
+				# for var in EP[:vNEW_TRANS_CAP_DECISION_INT]
+				# 	if value(var) > 0
+				# 		#println(var)
+				# 		total_builds[1] += value(var)
+				# 	end
+				# end
+				# println("NUMBER OF BUILDS IS ", total_builds[1])
 			end
 		else
 			println("The planning problem solution failed, trying with BarHomogenous=1")
 			set_attribute(EP, "BarHomogeneous", 1)
 			optimize!(EP)
 			if has_values(EP)
-				total_builds = [0.]
-				for var in EP[:vNEW_TRANS_CAP_DECISION_INT]
-					if value(var) > 0
-						#println(var)
-						total_builds[1] += value(var)
-					end
-				end
-				println("NUMBER OF BUILDS IS ", total_builds[1])
+				# total_builds = [0.]
+				# for var in EP[:vNEW_TRANS_CAP_DECISION_INT]
+				# 	if value(var) > 0
+				# 		#println(var)
+				# 		total_builds[1] += value(var)
+				# 	end
+				# end
+				# println("NUMBER OF BUILDS IS ", total_builds[1])
 				
 				zone_inv_cost = 0#make_benders_zonal_invcost(inputs, EP)
 				planning_sol =  (LB = objective_value(EP), inv_cost =value(EP[:eObj]), zone_inv_cost = zone_inv_cost,values =Dict([s=>value.(variable_by_name(EP,s)) for s in planning_variables]), theta = value.(EP[:vTHETA])) 
