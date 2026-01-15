ce62ca66df2f34e43919aba21620ee47395960da
David Cole <dlcdavidc@gmail.com>
2026-01-15 14:55:18 -0500

    Added 2x RTS system with new settings

--- example_systems/RTS_Case_Latest/TDR_benders_16repweeks_RTS_scale_up_sampledlines1_BD.jl
+++ example_systems/RTS_Case_Latest/TDR_benders_16repweeks_RTS_scale_up_sampledlines1_BD.jl
@@ -301,10 +301,13 @@ mysetup["BD_integer_routine"] = 1
 mysetup["BD_cap_integer_routine"] = 0
 mysetup["BD_warmstart_bilinear"] = 0
 mysetup["BD_warmstart_bigM"] = 1
-mysetup["BD_warmstart_bigM_maxiter"] = 300
+mysetup["BD_warmstart_bigM_maxiter"] = 700
+mysetup["BD_integer_routine_maxiter"] = 500
+mysetup["BD_post_warmstart_integer_routine_maxiter"] = 900
+mysetup["BD_post_warmstart_integer_routine"] = parse(Int, ARGS[1])
 mysetup["unfix_slacks"] = 1
 mysetup["BD_MaxIter"] = 3000
-mysetup["BD_MaxCpuTime"] = 39600
+mysetup["BD_MaxCpuTime"] = 115200
 
 myinputs_decomp = GenX.separate_inputs_subperiods(myinputs_copy);
 benders_inputs = GenX.generate_benders_inputs(mysetup,myinputs_copy,myinputs_decomp)
--- run_TDR_Benders_sampled_lines1_16repweeks_warmstart_RTS_scale_up.sh
+++ run_TDR_Benders_sampled_lines1_16repweeks_warmstart_RTS_scale_up.sh
@@ -1,15 +1,15 @@
 #!/bin/bash                                              
-#SBATCH --job-name=RTS_TDR_Benders_16repweeks_warmstart_RTS_scale_up_sampledlines1_integer_routine     # create a short name for your job
+#SBATCH --job-name=RTS_TDR_Benders_16repweeks_warmstart_RTS_scale_up_sampledlines1     # create a short name for your job
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=32       # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --output=slurm-%j.out
 #SBATCH --mem-per-cpu=6G       # memory per cpu-core
-#SBATCH --time=14:30:00          # total run time limit (HH:MM:SS)
+#SBATCH --time=36:00:00          # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=all          # send email when job ends
 #SBATCH --mail-user=dc0173@princeton.edu
 
 module purge
 module load gurobi/12.0.0
 module load julia/1.10.5
-julia -t 32 --project=./ ./example_systems/RTS_Case_Latest/TDR_benders_16repweeks_RTS_scale_up_sampledlines1.jl
+julia -t 32 --project=./ ./example_systems/RTS_Case_Latest/TDR_benders_16repweeks_RTS_scale_up_sampledlines1.jl 0
