e04e2bde7172fddfa26b418d6dd03c6cd803ecf4
David Cole <dlcdavidc@gmail.com>
2026-01-28 16:38:50 -0500

    Added script for getting locations for parsing

---
+++ example_systems/RTS_Case_Latest/Monolithic_LP_Solve_for_parsing_locations.jl
@@ -0,0 +1,165 @@
+ENV["GENX_PRECOMPILE"] = "false"
+
+using Revise
+using JuMP
+using GenX
+using PowerSystemsInvestmentsPortfolios
+using Gurobi
+using TimeSeries
+using CSV
+using DataFrames
+using Dates
+using InfrastructureSystems
+using PowerSystems
+const PSIP = PowerSystemsInvestmentsPortfolios
+const IS = InfrastructureSystems
+const PSY = PowerSystems
+using Plots
+import Pkg
+using Distributed, ClusterManagers
+using Random
+
+
+# Load in portfolio
+include((@__DIR__)*"/load_portfolio.jl")
+# Load in functions for downscaling
+include((@__DIR__)*"/../convert_input_dict_reconductoring.jl")
+# Load in functions for adding candidate lines
+include((@__DIR__)*"/load_candidate_line_functions.jl")
+
+# Set internal portfolio data for use in GenX
+p.internal.ext["Rep_Periods"] = 1
+p.internal.ext["Timesteps_per_Rep_Period"] = 168
+p.internal.ext["hours_per_subperiod"] = 168
+p.internal.ext["sub_weights"] = [8784 for i in 1:p.internal.ext["Rep_Periods"]] 
+add_om_costs(p)
+
+techs = collect(get_technologies(SupplyTechnology, p))
+
+# Build map of buses to zones; used in downscaling
+buses = collect(get_components(Bus, p.base_system))
+zones = []
+zone_map = Dict{Int, Int}()
+for i in 1:length(buses)
+    if buses[i].area.name == "area1"
+        push!(zones, 1)
+        zone_map[i] = 1
+    elseif buses[i].area.name == "area2"
+        push!(zones, 2)
+        zone_map[i] = 2
+    elseif buses[i].area.name == "area3"
+        push!(zones, 3)
+        zone_map[i] = 3
+    else
+        error()
+    end
+end
+
+# Load in settings
+genx_settings = GenX.get_settings_path(case, "genx_settings.yml") # Settings YAML file path
+writeoutput_settings = GenX.get_settings_path(case, "output_settings.yml") # Write-output settings YAML file path
+mysetup = GenX.configure_settings(genx_settings, writeoutput_settings) # mysetup dictionary stores settings and GenX-specific parameters
+
+# Make sure certain parameters are set
+mysetup["ParameterScale"] = 0
+mysetup["DC_OPF"] = 0
+mysetup["ptdf"] = 0
+mysetup["bilinear"] = 0
+mysetup["disaggregate"] = 0
+mysetup["unfix_slacks"] = 0
+mysetup["SOS1"] = 0
+settings_path = GenX.get_settings_path(case)    
+mysetup["settings_path"] = settings_path;
+mysetup["NetworkExpansion"] = 1
+mysetup["Benders"] = 0
+
+# node_names = ["Carew", "Chase", "Carrel", "Carter", "Cabot", "Bajer", "Baker", "Baffin", "Cabell", "Caine", "Camus", "Bach", "Bain", "Barlow", "Banks", "Balch", "Alger", "Alber", "Alder", "Avery", "Aiken"]
+
+# # Split node names by initial letter (A, B, C)
+# a_node_names = filter(n -> startswith(n, "A"), node_names)
+# b_node_names = filter(n -> startswith(n, "B"), node_names)
+# c_node_names = filter(n -> startswith(n, "C"), node_names)
+
+# # Set a reproducible seed (outside the function)
+# function sample_four(names::AbstractVector{<:AbstractString})
+#     @assert length(names) >= 3 "Need at least 4 names (got $(length(names)))"
+#     idxs = sort(randperm(length(names))[1:4])
+#     return collect(names[idxs])
+# end
+# node_name_dict = Dict('A' => a_node_names, 'B' => b_node_names, 'C' => c_node_names)
+# techs = collect(get_technologies(ResourceTechnology, p))
+
+# Random.seed!(1234)
+# for i in 1:length(techs)
+#     t = techs[i]
+#     if length(t.region) > 1
+#         old_region_list = t.region
+#         new_region_list = PSIP.RegionTopology[]
+#         key = t.region[1].name[1]
+#         node_name_vector = node_name_dict[key]
+#         new_nodes = sample_four(node_name_vector)
+#         for name in new_nodes
+#             for n in old_region_list
+#                 if n.name == name
+#                     push!(new_region_list, n)
+#                     break
+#                 end 
+#             end
+#         end
+#         @assert length(new_region_list) >=1
+#         t.region = new_region_list
+#     end
+# end
+
+myinputs = GenX.load_inputs(mysetup, case, p)
+
+optimizer = optimizer_with_attributes(Gurobi.Optimizer, "TimeLimit" => 64800, "MIPGap" => 1e-3)
+
+# Add expected candidate line data
+# also scales demands up by 2x
+load_candidates_base(myinputs, 8784, add_new_corridors = true)
+update_fuel_and_investment_costs(myinputs)
+
+# Run TDR
+# TDR_params = Dict("MinPeriods" => 16, "MaxPeriods" => 16, "UseExtremePeriods" => 1)
+# cluster_inputs(case, settings_path, mysetup; inputs = myinputs, TDR_params = TDR_params)
+
+GenX.expand_new_cap_resources_to_nodal!(myinputs, mysetup, p, "")
+
+if haskey(mysetup, "IntegerInvestments")
+    if mysetup["IntegerInvestments"] == 1
+        for i in myinputs["NEW_CAP"]
+            resource = myinputs["RESOURCES"][i]
+            parent(resource)[:cap_size] = 1
+        end
+    end
+end
+
+
+mysetup["IntegerInvestments"] = 1
+mysetup["DC_OPF"] = 0
+mysetup["NetworkExpansion"] = 1
+mysetup["ptdf"] = 0
+mysetup["bilinear"] = 0
+mysetup["Benders"] = 0
+mysetup["unfix_slacks"] = 0
+
+m = GenX.generate_model(mysetup, myinputs, optimizer)
+
+relax_integrality(m)
+optimize!(m)
+
+
+for i in myinputs["NEW_CAP"]
+    if value(m[:vCAP][i]) > .001
+        println(value(m[:vCAP][i]), "   ", GenX.region(myinputs["RESOURCES"][i]).name)
+    end
+end
+
+for v in m[:vCAP]
+    println(v, "   ", value(v))
+end
+
+for v in m[:vNEW_TRANS_CAP_DECISION_INT]
+    println(v, "   ", value(v))
+end
