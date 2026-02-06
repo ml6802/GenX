89bf6a26996fe560b28c039dfccc6057525d0d4d
David Cole <dlcdavidc@gmail.com>
2026-02-06 10:44:01 -0500

    Added print statement to debug Benders

--- src/benders/benders_subproblems.jl
+++ src/benders/benders_subproblems.jl
@@ -180,8 +180,12 @@ function solve_subproblem(EP::Model,planning_sol::NamedTuple,planning_variables_
         println("CANDIDATE FLOW EXPRESSION = ", EP[:cCANDFLOW].data[1,1])
     elseif haskey(EP, :cPOWER_FLOW_OPF_RETIRE_FORWARD)
         println("POWER FLOW EXPRESSION = ", EP[:cPOWER_FLOW_OPF_RETIRE_FORWARD].data[1,1])
+    else
+        println("NO FLOW EXPRESSIONS FOUND")
     end
 
+    flush(stdout)
+    
 	t = @elapsed optimize!(EP)
 	println("Time for solving subproblem was ", t/60, " minutes")
 	flush(stdout)
