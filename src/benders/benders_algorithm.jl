e17d9aec4983e935821bbeadad71be1f0a6f4571
David Cole <dlcdavidc@gmail.com>
2026-02-07 21:15:23 -0500

    Added src code for linear to bilinear code

--- src/benders/benders_algorithm.jl
+++ src/benders/benders_algorithm.jl
@@ -64,6 +64,16 @@ function benders(benders_inputs::Dict{Any,Any},setup::Dict,inputs)
 		LP_fallback_procedure = false
 	end
 
+	if !haskey(setup, "BD_linear_to_bilinear_switch")
+		setup["BD_linear_to_bilinear_switch"] = 0
+		linear_to_bilinear_switch = false
+	elseif setup["BD_linear_to_bilinear_switch"] == 1
+		linear_to_bilinear_switch = true
+	else
+		linear_to_bilinear_switch = false
+	end
+	
+
 	# if !haskey(setup, "BD_cap_integer_routine")
 	# 	setup["BD_cap_integer_routine"] = 0
 	# 	cap_integer_routine = false
@@ -390,14 +400,15 @@ function benders(benders_inputs::Dict{Any,Any},setup::Dict,inputs)
 
 				# go through the bilinear problem and remove slacks and add bilinear variables
 				p_id = workers();
-    			np_id = length(p_id);
+       			np_id = length(p_id);
 				t = @elapsed begin
-    			@sync for k in 1:np_id
-    			    @async @fetchfrom p_id[k] reset_subproblem_vector_to_linear(localpart(subproblems), inputs) #solve_local_subproblem(localpart(EP_subproblems),planning_sol,inputs); ### This is equivalent to fetch(@spawnat p .....)
-    			end
+				@sync for k in 1:np_id
+					@async @fetchfrom p_id[k] reset_subproblem_vector_to_linear(localpart(subproblems), inputs) #solve_local_subproblem(localpart(EP_subproblems),planning_sol,inputs); ### This is equivalent to fetch(@spawnat p .....)
+				end
 				end
 				println("TIME TO RESET SUBPROBLEMS WAS ", t / 60, " MINUTES")
 				solver_start_time[1] = solver_start_time[1] - t
+
 				UB = Inf
 				warmstart_linear_routine = false
 				if post_warmstart_integer_routine
@@ -427,6 +438,28 @@ function benders(benders_inputs::Dict{Any,Any},setup::Dict,inputs)
 				println()
 				println()
 				println()
+
+				if linear_to_bilinear_switch
+					t = @elapsed begin
+					if haskey(inputs, "inputs_decomp")
+						inputs_decomp = inputs["inputs_decomp"]
+						setup["unfix_slacks"] = 0
+						setup["DC_OPF"] = 1
+						setup["bilinear"] = 1
+						planning_variables = benders_inputs["planning_variables"]
+						subproblems_dist,planning_variables_sub = init_dist_subproblems(setup,inputs_decomp,planning_variables);
+						benders_inputs["subproblems"] = subproblems_dist
+						subproblems = subproblems_dist
+					else
+						@sync for k in 1:np_id
+							@async @fetchfrom p_id[k] reset_subproblem_vector_to_bilinear(localpart(subproblems), inputs) #solve_local_subproblem(localpart(EP_subproblems),planning_sol,inputs); ### This is equivalent to fetch(@spawnat p .....)
+						end
+    				end
+					end
+					println("TIME TO RESET SUBPROBLEMS WAS ", t / 60, " MINUTES")
+					solver_start_time[1] = solver_start_time[1] - t
+				end
+
 				set_integer.(integer_variables)
 				set_binary.(binary_variables)
 				LB = planning_sol.LB;
