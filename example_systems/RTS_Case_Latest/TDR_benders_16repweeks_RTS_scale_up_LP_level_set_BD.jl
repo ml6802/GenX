4dd71dbd66f7f3508dbdd4a846e579ccb1fe1d02
David Cole <dlcdavidc@gmail.com>
2026-02-09 11:16:58 -0500

    Added scale up test for regularization switch

--- example_systems/RTS_Case_Latest/TDR_benders_16repweeks_RTS_scale_up_LP_level_set_BD.jl
+++ example_systems/RTS_Case_Latest/TDR_benders_16repweeks_RTS_scale_up_LP_level_set_BD.jl
@@ -286,7 +286,8 @@ mysetup["BD_post_warmstart_integer_routine"] = 1
 mysetup["BD_Stab_Method"] = "int_level_set"
 mysetup["unfix_slacks"] = 1
 mysetup["BD_MaxIter"] = 3000
-mysetup["BD_MaxCpuTime"] = 115200
+mysetup["BD_MaxCpuTime"] = 43200
+mysetup["BD_regularization_switch"] = 1
 
 myinputs_decomp = GenX.separate_inputs_subperiods(myinputs_copy);
 benders_inputs = GenX.generate_benders_inputs(mysetup,myinputs_copy,myinputs_decomp)
