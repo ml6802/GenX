7107092610615fbfcb07e463e127c0b13f0efbb6
Mike Lau <ml6802@princeton.edu>
2026-02-12 12:31:26 -0800

    fix output bug

--- src/write_outputs/benders/write_benders_output.jl
+++ src/write_outputs/benders/write_benders_output.jl
@@ -372,6 +372,7 @@ function write_benders_mga_results!(Results_df::DataFrame, Costs_df::DataFrame, 
 	flow_df[!,:MGAIteration] .= iterations
 	power_df[!,:MGAIteration] .= iterations
 	
+	outpath = joinpath(path, "Outputs")
     CSV.write(joinpath(outpath, "SummaryMGA.csv"),Results_df)
     CSV.write(joinpath(outpath, "SummaryMGATimes.csv"),sumtime_df)
 	CSV.write(joinpath(outpath, "AnnualPowerByGen.csv"),power_df)
