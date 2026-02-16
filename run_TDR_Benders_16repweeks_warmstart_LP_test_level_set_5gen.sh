253700394b2587200b5169bc6ed199b1b7afd750
David Cole <dlcdavidc@gmail.com>
2026-02-16 18:58:10 -0500

    Added proper level set tests

---
+++ run_TDR_Benders_16repweeks_warmstart_LP_test_level_set_5gen.sh
@@ -0,0 +1,15 @@
+#!/bin/bash                                              
+#SBATCH --job-name=RTS_TDR_Benders_16repweeks_warmstart_LP_level_set_5gen     # create a short name for your job
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=32       # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --output=slurm-%j.out
+#SBATCH --mem-per-cpu=5G       # memory per cpu-core
+#SBATCH --time=24:00:00          # total run time limit (HH:MM:SS)
+#SBATCH --mail-type=all          # send email when job ends
+#SBATCH --mail-user=dc0173@princeton.edu
+
+module purge
+module load gurobi/12.0.0
+module load julia/1.10.5
+julia -t 32 --project=./ ./example_systems/RTS_Case_Latest/TDR_benders_16repweeks_warmstart_LP_test_level_set_5gen.jl 1 1
